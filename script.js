7bfda5fbd614ddb56b7495525f5de3db09faef28
prateekpkulkarni <157222496+prateekpkulkarni@users.noreply.github.com>
2024-02-27 10:05:20 +0530

    Create script.js
    Signed-off-by: prateekpkulkarni <157222496+prateekpkulkarni@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const modeToggle = document.getElementById('mode-toggle');
+
+  modeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+  });
+});
